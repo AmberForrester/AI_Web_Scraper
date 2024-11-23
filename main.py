2166b480f2358b76e10a53a90e818035da5542a0
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-23 01:22:33 -0500

    Cleaning DOM Content

--- main.py
+++ main.py
@@ -6,13 +6,30 @@ st.set_page_config(
 )
 
 
-from scrape import scrape_website
+from scrape import (
+    scrape_website, 
+    split_dom_content, 
+    clean_body_content, 
+    extract_body_content
+)
+
 
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
-if st.button("Scrape Site"):
-    st.write("Scraping the website...")
-    result = scrape_website(url)
-    print(result)
+# Step 1: Scrape the Website:
+if st.button("Scrape Website"):
+    if url:
+        st.write("Scraping the website...")
+    
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
+        
+        # Store DOM content in Streamlit session state:
+        st.session_state.dom_content = cleaned_content
+        
+        # Display the DOM content in an expandable text box to toggle content: 
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", cleaned_content, height=300)    
