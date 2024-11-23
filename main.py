ea1ece43f890a5ebb61826fb8b7cbfd4ae66cbb5
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-23 00:50:55 -0500

    Web Scraping w/Selenium + Favicon Added

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import streamlit as st 
 
+st.set_page_config(
+    page_title = "AI Web Scraper",
+    page_icon = "images/favicon.ico"
+)
+
+
+from scrape import scrape_website
+
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping the website...")
+    result = scrape_website(url)
+    print(result)
